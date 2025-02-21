888fc15ea53a2b91587c0e672b2d630c57844469
Fast&Safe99@Code <renukarajaput123@gmail.com>
2025-02-21 10:37:41 +0530

    dynamic routes

---
+++ src/app/products/[productId]/page.tsx
@@ -0,0 +1,6 @@
+export default function ProductList1({params} : {params :{productId : string}})
+{
+return(
+    <div><h1>peoduct : {params.productId} </h1></div>
+)
+}
---
+++ src/app/products/page.tsx
@@ -0,0 +1,11 @@
+export default function ProductList()
+{
+    return (
+        <div>
+            <h1>ProductList</h1>
+            <h2>product 1</h2>
+            <h3>product 2</h3>
+            
+        </div>
+    )
+}
